66802ecc86ac40c8f63f9305f9e3e02e837f9d09
Paul Battley <pbattley@gmail.com>
2015-07-23 11:33:09 +0100

    Correct AssaultOffence

--- app/models/assault_offence.rb
+++ app/models/assault_offence.rb
@@ -3,6 +3,6 @@ class AssaultOffence
 
   attribute :type, String, writer: :private, default: 'AssaultOffence'
   attribute :cjs_offence_code, AssaultCjsCode
-  attribute :assault_interview_extension, RetailTheftInterviewExtension
+  attribute :assault_interview_extension, AssaultInterviewExtension
   attribute :victim_name, PersonName
 end
