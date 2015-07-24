60aad0684d5966ad91b98e971f37cb55733f7e3a
Paul Battley <pbattley@gmail.com>
2015-07-24 16:58:03 +0100

    Correct CjsOffenceCode

--- app/models/retail_theft_offence.rb
+++ app/models/retail_theft_offence.rb
@@ -1,6 +1,6 @@
 class RetailTheftOffence < Offence
   attribute :type, String, writer: :private, default: 'RetailTheftOffence'
-  attribute :cjs_offence_code, String
+  attribute :cjs_offence_code, RetailCjsCode
   attribute :property_ids, Array[Integer]
   attribute :interview_extension, RetailTheftInterviewExtension
 end
