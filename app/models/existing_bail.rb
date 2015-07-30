44b06dc12b3b9b0604b3f98daf7044c8d32ffae9
Paul Battley <pbattley@gmail.com>
2015-07-30 15:46:19 +0100

    Correct fields that had wrong type

--- app/models/existing_bail.rb
+++ app/models/existing_bail.rb
@@ -3,9 +3,9 @@ class ExistingBail
   include ActiveModel::Validations
 
   attribute :original_charge_wording, Array[String]
-  attribute :bailed_from, String
+  attribute :bailed_from, OrganisationLocation
   attribute :bailed_date, Date
-  attribute :to_appear_at, String
+  attribute :to_appear_at, OrganisationLocation
   attribute :datetime_to_appear, DateTime
   attribute :list_of_conditions, Array[BailCondition]
 end
