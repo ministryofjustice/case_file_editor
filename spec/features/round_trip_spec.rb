60211a0b0fbcd1b276a67feec45d376dadad31e1
Paul Battley <pbattley@gmail.com>
2015-08-04 16:18:40 +0100

    Test round-trip processing of a case file

---
+++ spec/features/round_trip_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Round-trip importing and exporting' do
+  let(:dcf) {
+    CaseImporter.new(source).import
+  }
+
+  let(:exported) {
+    CaseExporter.new(dcf).export
+  }
+
+  context 'example_1.json' do
+    let(:source) {
+      JSON.load(
+        File.read(
+          File.expand_path('../../fixtures/example_1.json', __FILE__)))
+    }
+
+    it 'produces the same structure after being imported and exported' do
+      expect(exported).to eq(source)
+    end
+  end
+end
