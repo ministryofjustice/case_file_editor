e8177d0a3b2ee71f21fb6fe37420698b594cb01c
Paul Battley <pbattley@gmail.com>
2015-09-11 17:42:31 +0100

    Test name importer

---
+++ spec/importers/name_importer_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe NameImporter do
+  subject {
+    described_class.importer(source).import
+  }
+
+  context 'when the object is a person name' do
+    let(:source) {
+      {
+        'given_name' => %w[ Jennie Claire ],
+        'surname' => 'Jones'
+      }
+    }
+
+    it { is_expected.to be_kind_of(PersonName) }
+
+    it 'has given_name' do
+      expect(subject.given_name).to eq(%w[ Jennie Claire ])
+    end
+
+    it 'has surname' do
+      expect(subject.surname).to eq('Jones')
+    end
+  end
+
+  context 'when the object is an organisation name' do
+    let(:source) {
+      {
+        'organisation_name' => 'ACME, Inc.'
+      }
+    }
+
+    it { is_expected.to be_kind_of(OrganisationName) }
+
+    it 'has organisation_name' do
+      expect(subject.organisation_name).to eq('ACME, Inc.')
+    end
+  end
+end
