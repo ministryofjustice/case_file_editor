13aeb571e0b11bc3a71410a0f08177eb5aa1beff
Paul Battley <pbattley@gmail.com>
2015-09-25 17:13:19 +0100

    Use fetch to fail fast in validators

--- app/validators/array_format_validator.rb
+++ app/validators/array_format_validator.rb
@@ -1,6 +1,6 @@
 class ArrayFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.all? { |v| options[:with].match(v) }
+    unless value.all? { |v| options.fetch(:with).match(v) }
       record.errors.add(attribute, :array_invalid, options)
     end
   end
--- app/validators/array_inclusion_validator.rb
+++ app/validators/array_inclusion_validator.rb
@@ -1,6 +1,6 @@
 class ArrayInclusionValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.all? { |g| options[:in].include?(g) }
+    unless value.all? { |g| options.fetch(:in).include?(g) }
       record.errors.add(attribute, :array_inclusion, options)
     end
   end
