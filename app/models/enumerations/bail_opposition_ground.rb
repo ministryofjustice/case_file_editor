4b8fd19cb08cdc79f60a98216eaf02d9266a857d
paulspinks2 <paul.spinks@medley.co.uk>
2015-10-28 17:11:38 +0000

    Domestic Violence Correction & Bail Opposition enumeration

--- app/models/enumerations/bail_opposition_ground.rb
+++ app/models/enumerations/bail_opposition_ground.rb
@@ -3,7 +3,7 @@ module Enumerations
     breach_of_bail
     failed_to_surrender_to_custody
     commit_offences_on_bail
-    interfere_with_witness_or_otherwise_obstrcut_the_course_of_justice
+    interfere_with_witness_or_otherwise_obstruct_the_course_of_justice
     for_the_defendants_own_protection
     for_the_defendants_own_welfare
     charged_with_or_convicted_of_a_violent_or_sexual_offence_equivalent_to_14_plus_years_imprisonment_in_the_case_of_an_adult
