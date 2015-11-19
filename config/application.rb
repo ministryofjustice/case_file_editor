b571004da12b01d6bd830702c59974271c677247
Paul Battley <pbattley@gmail.com>
2015-11-19 11:17:30 +0000

    Update supported version to 1.2.0 draft 4

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Dcf
     config.middleware.
       insert_before ActionDispatch::ParamsParser, 'CatchJsonParseErrors'
 
-    config.supported_version = '1.1.0'
+    config.supported_version = '1.2.0 draft 4'
 
     # Settings in config/environments/* take precedence over those specified
     # here.
