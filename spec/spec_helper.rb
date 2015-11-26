aba2e916571b18234be1a9fec0f63707a950e5e6
Paul Battley <pbattley@gmail.com>
2015-11-26 16:16:55 +0000

    Abandon quick loading in specs
    It causes segfaults.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,6 @@ require 'simplecov-rcov'
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.minimum_coverage 100
 
-# Minimal auto-load for quicker specs. This avoids loading the whole of Rails
-# solely for dependency resolution.
-autoload :ActiveModel, 'active_model'
-autoload :Virtus, 'virtus/relations'
-require 'active_support'
-Dir[File.expand_path('../../{lib,app/**/*}', __FILE__)].each do |path|
-  next unless File.directory?(path)
-  ActiveSupport::Dependencies.autoload_paths << path
-end
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
