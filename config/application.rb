739ebf564a3c6b3ce653d45d901d2ad33e8a9bf6
Paul Spinks <paul.spinks@medley.co.uk>
2015-12-14 12:33:29 -0500

    Updated version, fixed application title

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Dcf
     config.middleware.
       insert_before ActionDispatch::ParamsParser, 'CatchJsonParseErrors'
 
-    config.supported_version = '1.2.0 draft 1'
+    config.supported_version = '1.2.0 draft 2'
 
     # Settings in config/environments/* take precedence over those specified
     # here.
