8160996696e193e36d02b5f225f43e3b9357159a
Paul Spinks <paul.spinks@medley.co.uk>
2016-01-14 11:15:21 -0500

    Updated Schema to 1.2.0.1 - Injuries fix

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Dcf
     config.middleware.
       insert_before ActionDispatch::ParamsParser, 'CatchJsonParseErrors'
 
-    config.supported_version = '1.2.0 Final'
+    config.supported_version = '1.2.0.1 Final'
 
     # Settings in config/environments/* take precedence over those specified
     # here.
