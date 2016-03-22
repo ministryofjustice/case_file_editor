a070e612f9306c6b4ce3751a2e0c1a2fec09e7a5
Paul Spinks <paul.spinks@medley.co.uk>
2016-03-22 09:18:08 -0400

    Corrections for 1.2.1.1 (Schema only) multimedia evidence response for BoB Victim object required for PersonVictim

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Dcf
     config.middleware.
       insert_before ActionDispatch::ParamsParser, 'CatchJsonParseErrors'
 
-    config.supported_version = '1.2.1.0'
+    config.supported_version = '1.2.1.1'
 
     # Settings in config/environments/* take precedence over those specified
     # here.
