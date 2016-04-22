8d845af6480b30bc6e674e2649420ebb9bcc102a
EC2 Default User <ec2-user@ip-172-31-51-143.ec2.internal>
2016-04-22 17:56:01 +0000

    Added additional files missing from previous commit

---
+++ app/models/enumerations/class_a_drug_test.rb
@@ -0,0 +1,8 @@
+module Enumerations
+  ClassADrugTest = %w[
+    not_requested
+    refused_to_provide
+    provided_positive
+    provided_negative
+  ]
+end
---
+++ app/models/enumerations/rep_wish.rb
@@ -0,0 +1,8 @@
+module Enumerations
+  RepWish = %w[
+    yes_cps_to_read
+    yes_rep_to_read
+    no_cps_to_read
+    no_cps_to_disclose_and_not_to_read
+  ]
+end
